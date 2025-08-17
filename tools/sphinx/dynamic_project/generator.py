a416182aab392ea97aa3e31470f1a536bf879115
Marco Heinemann <marco.heinemann@useblocks.com>
2025-08-18 00:36:32 +0200

    abs needimport path

--- tools/sphinx/dynamic_project/generator.py
+++ tools/sphinx/dynamic_project/generator.py
@@ -106,20 +106,29 @@ def generate_sphinx_structure(
                 # if project_idx:
                 #     project_name = path_parts[project_idx]
                 #     # Create relative path to the needs.json file
-                relative_dir = f"../../../../../../../{needs_json_path}"
-                relative_path = f'{relative_dir}/needs.json'
+                # relative_dir = f"../../../{needs_json_path}"
+                # relative_path = f"{relative_dir}/needs.json"
 
-                to_print = Path(index_path).resolve().parent.parent.parent.parent.parent.parent.parent / needs_json_path
-                to_print = Path(index_path.parent).parent.resolve()
-                print(f"Files in directory {to_print}:")
-                if to_print.exists() and to_print.is_dir():
-                    for file in to_print.iterdir():
-                        print(f"  {file}")
-                else:
-                    print(f"  Directory does not exist or is not a directory")
+                bazel_root_abs = index_path.parent.parent.parent.parent.resolve()
+                json_builder_dir = bazel_root_abs / needs_json_path
+                # print(f"Files in directory {json_builder_dir}:")
+                # if json_builder_dir.exists() and json_builder_dir.is_dir():
+                #     for file in json_builder_dir.iterdir():
+                #         print(f"  {file}\n")
+                # else:
+                #     print("  Directory does not exist or is not a directory")
+                needs_json_path = json_builder_dir / "needs.json"
+                # if needs_json_path.exists():
+                #     print(f"Contents of {needs_json_path}:")
+                #     with open(needs_json_path, "r") as f:
+                #         print(f.read())
+                # else:
+                #     print(f"File {needs_json_path} does not exist")
 
-                # raise ValueError(relative_path)
-                needimport_entries.append(f".. needimport:: {relative_path}\n")
+                import_title = f'{needs_json_file}/needs.json'
+                needimport_entries.append(f'{import_title}')
+                needimport_entries.append('^' * len(import_title) + '\n')
+                needimport_entries.append(f".. needimport:: /{needs_json_path}\n")
 
     # Replace placeholders in template
     toctree_content = "\n   ".join(toctree_entries)
